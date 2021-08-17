5bb0e16a0df7eccc37c14df9c8e4e8d2520886ad
Tsogoo <ts.r.madrid@gmail.com>
2021-08-17 17:56:47 +0800

    Fix search page error

--- lib/api/resources/product.ts
+++ lib/api/resources/product.ts
@@ -1,72 +1,78 @@
-import { IProduct } from '../../../types/resources/productTypes';
+import { IProduct } from "../../../types/resources/productTypes";
 import { ITaxon } from "../../../types/resources/taxonTypes";
 import { getResource } from "../client";
 import { getLocale } from "../../config/locales";
 
 export const getAllProducts = async (): Promise<IProduct[]> => {
-    const products = await getResource('shop/products', null, {
-        'enabled': true,
-        'itemsPerPage': 9999999,
-        'variants.onHand[gt]': 0
-    });
-    
-    return products;
-}
+  const products = await getResource("shop/products", null, {
+    enabled: true,
+    itemsPerPage: 9999999,
+    "variants.onHand[gt]": 0,
+  });
+
+  return products;
+};
 
 export const searchProducts = async (search: string): Promise<IProduct[]> => {
-    const products = await getResource('shop/products/search/{search}', {
-            'search': search
-        },
-        {
-            'enabled': true,
-            'itemsPerPage': 9999999,
-            'variants.onHand[gt]': 0
-        }
-    );
-
-
-    return products;
-}
-
-export const getLastProducts = async (limit: number): Promise<IProduct[]> => {
-    const products = await getResource('shop/products', null, {
-        'page': 1,
-        'itemsPerPage': limit,
-        'enabled': true,
-        'variants.onHand[gt]': 0
-    });
-
-    return products;
-}
-
-export const getProductByTranslatedSlug = async(slug: string): Promise<IProduct> => {
-    const product = await getResource('shop/products/by-slug/{slug}/{locale}', {
-        'slug': slug,
-        locale: getLocale(),
+  const products = await getResource(
+    "shop/products/search/{search}/{locale}",
+    {
+      search: search,
+      locale: getLocale(),
     },
     {
-        'enabled': true,
-        'itemsPerPage': 9999999,
-        'variants.onHand[gt]': 0
+      enabled: true,
+      itemsPerPage: 9999999,
+      "variants.onHand[gt]": 0,
     }
-);
-    return product;
-}
+  );
 
-export const getProductsByTaxon = async (taxon: ITaxon | null): Promise<IProduct[] | null> => {
+  return products;
+};
 
-    let products = null;
+export const getLastProducts = async (limit: number): Promise<IProduct[]> => {
+  const products = await getResource("shop/products", null, {
+    page: 1,
+    itemsPerPage: limit,
+    enabled: true,
+    "variants.onHand[gt]": 0,
+  });
 
-    if (taxon) {
-        products = await getResource('shop/products', null, {
-            'productTaxons.taxon.code': taxon.code,
-            'enabled': true,
-            'variants.onHand[gt]': 0,
-            'order[createdAt]': 'desc',
-            'itemsPerPage': 9999999,
-        });
+  return products;
+};
+
+export const getProductByTranslatedSlug = async (
+  slug: string
+): Promise<IProduct> => {
+  const product = await getResource(
+    "shop/products/by-slug/{slug}/{locale}",
+    {
+      slug: slug,
+      locale: getLocale(),
+    },
+    {
+      enabled: true,
+      itemsPerPage: 9999999,
+      "variants.onHand[gt]": 0,
     }
+  );
+  return product;
+};
 
+export const getProductsByTaxon = async (
+  taxon: ITaxon | null
+): Promise<IProduct[] | null> => {
+  let products = null;
 
-    return products;
-}
+  if (taxon) {
+    products = await getResource("shop/products", null, {
+      "productTaxons.taxon.code": taxon.code,
+      enabled: true,
+      "variants.onHand[gt]": 0,
+      "order[createdAt]": "desc",
+      itemsPerPage: 9999999,
+    });
+  }
+
+  return products;
+};
